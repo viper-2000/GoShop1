955d9812185e19d8523faace2afedd495333380e
Aishwarya121901 <aishwaryasreev@gmail.com>
2021-04-23 10:13:24 +0530

    second commit

--- app/src/main/java/com/example/goshop/MainActivity.java
+++ app/src/main/java/com/example/goshop/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.goshop;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button joinNowButton, loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +23,23 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton = (Button) findViewById(R.id.main_join_now);
         loginButton = (Button) findViewById(R.id.main_login_button);
+
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, loginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, registeractivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
